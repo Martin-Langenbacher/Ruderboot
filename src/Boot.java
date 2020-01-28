5a6141fbfa796bad2492855a52c026bf43d2950f
Martin Langenbacher <martin.langenbacher@googlemail.com>
2020-01-28 17:44:09 +0100

    boat with different speed in the race - by luck, e.g. wrong start...

--- src/Boot.java
+++ src/Boot.java
@@ -1,7 +1,8 @@
+import java.util.ArrayList;
 
-public abstract class Boot implements Comparable<Boot>{
-	
-	//attributes
+public abstract class Boot implements Comparable<Boot> {
+
+	// attributes
 	private int number;
 	private String bootName;
 	private int xPos;
@@ -18,18 +19,14 @@ public abstract class Boot implements Comparable<Boot>{
 	private double streckeMade;
 	private int position;
 	private boolean sortierSchalter;
+	private boolean fehlstart;
+	private boolean zwischenspurt;
+	private boolean endspurt;
+	private boolean leistungsEinbruch;
 
-	
-	
-	
-	
 	// constructor
-	public Boot(int number, String bootName, int leistungBoot,
-			String backBoardRuderVorn,
-			String backBoardRuderHinten,
-			String bootMitte,
-			String steuerBoardRuderVorn,
-			String steuerBoardRuderHinten) {
+	public Boot(int number, String bootName, int leistungBoot, String backBoardRuderVorn, String backBoardRuderHinten,
+			String bootMitte, String steuerBoardRuderVorn, String steuerBoardRuderHinten) {
 		this.number = number;
 		this.bootName = bootName;
 		this.leistungBoot = leistungBoot;
@@ -45,119 +42,79 @@ public abstract class Boot implements Comparable<Boot>{
 		this.position = 0;
 		this.bahnNummer = number + 1;
 		this.sortierSchalter = false;
+		this.fehlstart = false;
+		this.zwischenspurt = false;
+		this.endspurt = false;
+		this.leistungsEinbruch = false;
 	}
-	
-	
+
 	
 	
 	// getter and setter
-
 	public int getNumber() {
 		return number;
 	}
-
-
-
 
 	public void setNumber(int number) {
 		this.number = number;
 	}
 
-	
-
 	public String getBootName() {
 		return bootName;
 	}
-
-
-
 
 	public void setBootName(String bootName) {
 		this.bootName = bootName;
 	}
 
-
-
-
 	public int getxPos() {
 		return xPos;
 	}
-
-
-
 
 	public void setxPos(int xPos) {
 		this.xPos = xPos;
 	}
 
-
-
-
 	public int getbahnNummer() {
 		return bahnNummer;
 	}
-	
-	
+
 	public void setBahnNummer(int bahnNummer) {
 		this.bahnNummer = bahnNummer;
 	}
-
-
-
 
 	public String getBackBoardRuderVorn() {
 		return backBoardRuderVorn;
 	}
 
-
-
-
 	public String getBackBoardRuderHinten() {
 		return backBoardRuderHinten;
 	}
-
-
-
 
 	public String getBootMitte() {
 		return bootMitte;
 	}
 
-
-
-
 	public String getSteuerBoardRuderVorn() {
 		return steuerBoardRuderVorn;
 	}
-
-
-
 
 	public String getSteuerBoardRuderHinten() {
 		return steuerBoardRuderHinten;
 	}
 
-
-
-
 	public int getLengthOfBoot() {
 		return lengthOfBoot;
 	}
-
-
-
 
 	public int getLeistungBoot() {
 		return leistungBoot;
 	}
 
-
-
-
 	public void setLeistungBoot(int leistungBoot) {
 		this.leistungBoot = leistungBoot;
 	}
-	
+
 	public boolean isRuderVorn() {
 		return ruderVorn;
 	}
@@ -165,125 +122,199 @@ public abstract class Boot implements Comparable<Boot>{
 	public void setRuderVorn(boolean ruderVorn) {
 		this.ruderVorn = ruderVorn;
 	}
-	
-	
 
 	public boolean isWinner() {
 		return winner;
 	}
 
-
-
 	public void setWinner(boolean winner) {
 		this.winner = winner;
 	}
-	
-	
-	
+
 	public double getStreckeMade() {
 		return streckeMade;
 	}
-
 
 	public void setStreckeMade(double streckeMade) {
 		this.streckeMade = streckeMade;
 	}
 
-
 	public int getPosition() {
 		return position;
 	}
-
 
 	public void setPosition(int position) {
 		this.position = position;
 	}
 
-
-public boolean isSortierSchalter() {
+	public boolean isSortierSchalter() {
 		return sortierSchalter;
 	}
-
-
-
 
 	public void setSortierSchalter(boolean sortierSchalter) {
 		this.sortierSchalter = sortierSchalter;
 	}
+	
+	public boolean isFehlstart() {
+		return fehlstart;
+	}
 
+	public void setFehlstart(boolean fehlstart) {
+		this.fehlstart = fehlstart;
+	}
 
+	public boolean isZwischenspurt() {
+		return zwischenspurt;
+	}
 
+	public void setZwischenspurt(boolean zwischenspurt) {
+		this.zwischenspurt = zwischenspurt;
+	}
 
-	//=======================================================================
+	public boolean isEndspurt() {
+		return endspurt;
+	}
+
+	public void setEndspurt(boolean endspurt) {
+		this.endspurt = endspurt;
+	}
+
+	public boolean isLeistungsEinbruch() {
+		return leistungsEinbruch;
+	}
+
+	public void setLeistungsEinbruch(boolean leistungsEinbruch) {
+		this.leistungsEinbruch = leistungsEinbruch;
+	}
+
+	public int getBahnNummer() {
+		return bahnNummer;
+	}
+
+	public void setBackBoardRuderVorn(String backBoardRuderVorn) {
+		this.backBoardRuderVorn = backBoardRuderVorn;
+	}
+
+	public void setBackBoardRuderHinten(String backBoardRuderHinten) {
+		this.backBoardRuderHinten = backBoardRuderHinten;
+	}
+
+	public void setBootMitte(String bootMitte) {
+		this.bootMitte = bootMitte;
+	}
+
+	public void setSteuerBoardRuderVorn(String steuerBoardRuderVorn) {
+		this.steuerBoardRuderVorn = steuerBoardRuderVorn;
+	}
+
+	public void setSteuerBoardRuderHinten(String steuerBoardRuderHinten) {
+		this.steuerBoardRuderHinten = steuerBoardRuderHinten;
+	}
+
+	public void setLengthOfBoot(int lengthOfBoot) {
+		this.lengthOfBoot = lengthOfBoot;
+	}
+	
+	
+	
+	
+	
+
+	// =======================================================================
 	public void bootEinsetzen(Rennstrecke rennstrecke) {
 		String boatAndTrackStringB = "";
 		String boatAndTrackStringM = "";
 		String boatAndTrackStringS = "";
 		String empty = " ";
-		
-		boatAndTrackStringM = " |" + empty.repeat((this.xPos-1) - this.lengthOfBoot) + this.getBootMitte() + empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos())+1) +"|";
+
+		boatAndTrackStringM = " |" + empty.repeat((this.xPos - 1) - this.lengthOfBoot) + this.getBootMitte()
+				+ empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos()) + 1) + "|";
 		if (this.isRuderVorn()) {
-			boatAndTrackStringB = " |" + empty.repeat((this.xPos-1) - this.lengthOfBoot) + this.getBackBoardRuderVorn() + empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos())+1) +"|";
-			boatAndTrackStringS = " |" + empty.repeat((this.xPos-1) - this.lengthOfBoot) + this.getSteuerBoardRuderVorn() + empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos())+1) +"|";
+			boatAndTrackStringB = " |" + empty.repeat((this.xPos - 1) - this.lengthOfBoot)
+					+ this.getBackBoardRuderVorn()
+					+ empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos()) + 1) + "|";
+			boatAndTrackStringS = " |" + empty.repeat((this.xPos - 1) - this.lengthOfBoot)
+					+ this.getSteuerBoardRuderVorn()
+					+ empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos()) + 1) + "|";
 		} else {
-			boatAndTrackStringB = " |" + empty.repeat((this.xPos-1) - this.lengthOfBoot) + this.getBackBoardRuderHinten() + empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos())+1) +"|";
-			boatAndTrackStringS = " |" + empty.repeat((this.xPos-1) - this.lengthOfBoot) + this.getSteuerBoardRuderHinten() + empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos())+1) +"|";
+			boatAndTrackStringB = " |" + empty.repeat((this.xPos - 1) - this.lengthOfBoot)
+					+ this.getBackBoardRuderHinten()
+					+ empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos()) + 1) + "|";
+			boatAndTrackStringS = " |" + empty.repeat((this.xPos - 1) - this.lengthOfBoot)
+					+ this.getSteuerBoardRuderHinten()
+					+ empty.repeat(rennstrecke.getLengthRaceTrack() - (this.getxPos()) + 1) + "|";
 		}
-		
-				
+
 		rennstrecke.setBackboardWater(boatAndTrackStringB);
 		rennstrecke.setMiddleStringBoat(boatAndTrackStringM);
 		rennstrecke.setStarboardWater(boatAndTrackStringS);
-		
-		setRuderVorn(!isRuderVorn());
-				 
-	}
 
-	
+		setRuderVorn(!isRuderVorn());
+
+	}
 
 //=======================================================================
 	public int compareTo(Boot compareBoot) {
-		
+
 		if (this.sortierSchalter == false) {
-			
+
 			int comparePosition = ((Boot) compareBoot).getLeistungBoot();
-			
-			//ascending order
-			//return this.leistungBoot - compareQuantity;
-			
-			//descending order
+
+			// ascending order
+			// return this.leistungBoot - compareQuantity;
+
+			// descending order
 			return comparePosition - this.leistungBoot;
 		} else {
-			
+
 			int compareBahnen = ((Boot) compareBoot).getbahnNummer();
-			
-			//ascending order;-)");
+
+			// ascending order;-)");
 			return this.bahnNummer - compareBahnen;
-			
-			//descending order
-			//return comparePosition - this.leistungBoot;
+
+			// descending order
+			// return comparePosition - this.leistungBoot;
 		}
-		
+
 	}
 
-
-
 //================================================================================
-	public double luckFaktor() {
-	
-		return 1.0;
+	public double luckFaktor(int lengthOfRacingField) {
 		
-	};
-	
+		double variableFehlstart;
+		double variableLeistungseinbruch;
+		double variableZwischenspurt;
+		double variableEndspurt;
+		
+		int x1 = (int) (0.30 * lengthOfRacingField);
+		int x2 = (int) (0.75 * lengthOfRacingField);
+		int x3 = (int) (0.80 * lengthOfRacingField);
+		
+		variableFehlstart = 1.0;
+		variableLeistungseinbruch = 1.0;
+		variableZwischenspurt = 1.0;
+		variableEndspurt = 1.0;
+			
+		
+		if (this.fehlstart && this.getxPos() <= this.getLengthOfBoot() + 2) {
+			variableFehlstart = 0.5;
+		}
+		
+		if (this.leistungsEinbruch && this.getxPos() > x2 && this.getxPos() < x3) {
+			variableLeistungseinbruch = 0.8;
+		}
+		
+		if (this.zwischenspurt && this.getxPos() > x1 && this.getxPos() < x2-1) {
+			variableZwischenspurt = 1.1;
+		}
+		
+		if (this.endspurt && this.getxPos() > x3 && this.getxPos() < lengthOfRacingField) {
+			variableEndspurt = 1.2;
+		}
 
-	
-		
-		
-	
+		return variableFehlstart * variableLeistungseinbruch * variableZwischenspurt * variableEndspurt;
+	}
 	
 	
 	
-
 }
-
-
--- src/Wettrennen.java
+++ src/Wettrennen.java
@@ -2,20 +2,22 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 public class Wettrennen {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
-		
+
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader br = new BufferedReader(isr);
-		
+
 		int amountOfBoats = 0;
 		int lengthOfRacingField = 100;
 		int leistungForBoat;
-		int amountOfRuderer = 1; // hier können 1er, 2er, 4er oder 8er stehen (neuere Version: Es wird abgefragt)...
+		int amountOfRuderer = 1; // hier können 1er, 2er, 4er oder 8er stehen (neuere Version: Es wird
+									// abgefragt)...
 		int zaehler = 0;
 		int bootNummerZaehler = 0;
 		int k = 0; // auch ein Zähler
@@ -24,9 +26,7 @@ public class Wettrennen {
 		String bootName;
 		double factorForSpeedOfBoat = 10.0;
 		double laengsteDistanzImRennen = 0.0;
-		
-		
-		
+
 		// Anzahl der Ruderboote.
 		while (zaehler == 0) {
 
@@ -43,48 +43,45 @@ public class Wettrennen {
 			zaehler++;
 
 		}
-		
-		
+
 		// Ruderer:
-				while (zaehler == 1) {
+		while (zaehler == 1) {
 
-					try {
-						System.out.println("Geben Sie die Zahl der Ruderer ein (1, 2, 4 oder 8):");
-						eingabe = br.readLine();
-						amountOfRuderer = Integer.parseInt(eingabe);
-					}
+			try {
+				System.out.println("Geben Sie die Zahl der Ruderer ein (1, 2, 4 oder 8):");
+				eingabe = br.readLine();
+				amountOfRuderer = Integer.parseInt(eingabe);
+			}
 
-					catch (NumberFormatException ausnahme) {
-						System.out.println("Bitte eine Zahl eingeben!");
-						continue;
-					}
-					zaehler++;
-				}
-								
-				if ((amountOfRuderer != 1) && (amountOfRuderer != 2) && (amountOfRuderer != 4) && (amountOfRuderer != 8)) {
-					amountOfRuderer = 4;
-					System.out.println("Falsche Zahl eingegeben, darum ist die Anzahl der Ruderer auf " +amountOfRuderer + " gesetzt worden.");
-				}
-				
-		
-			
+			catch (NumberFormatException ausnahme) {
+				System.out.println("Bitte eine Zahl eingeben!");
+				continue;
+			}
+			zaehler++;
+		}
+
+		if ((amountOfRuderer != 1) && (amountOfRuderer != 2) && (amountOfRuderer != 4) && (amountOfRuderer != 8)) {
+			amountOfRuderer = 4;
+			System.out.println("Falsche Zahl eingegeben, darum ist die Anzahl der Ruderer auf " + amountOfRuderer
+					+ " gesetzt worden.");
+		}
+
 		// erstelle Boote
 		ArrayList<Boot> boote = new ArrayList<>();
 		for (int i = 0; i < amountOfBoats; i++) {
-			
+
 			leistungForBoat = 0;
-			//erstelle Leistung für das Boot, zufällig, pro Ruderer zwischen 400 und 500 Watt
+			// erstelle Leistung für das Boot, zufällig, pro Ruderer zwischen 400 und 500
+			// Watt
 			for (int j = 0; j < amountOfRuderer; j++) {
-				
-				int randomWatt = (int)(Math.random() * 100 + 400);
-				//System.out.println(randomWatt);
+
+				int randomWatt = (int) (Math.random() * 100 + 400);
 				leistungForBoat = leistungForBoat + randomWatt;
 			}
-			//System.out.println("Summe: " +leistungForBoat);
-			
-			bootNummerZaehler = bootNummerZaehler +1;
-			
-			switch(amountOfRuderer) {
+
+			bootNummerZaehler = bootNummerZaehler + 1;
+
+			switch (amountOfRuderer) {
 			case 1:
 				bootName = "Einer " + bootNummerZaehler;
 				boote.add(new Einer(i, bootName, leistungForBoat));
@@ -107,72 +104,61 @@ public class Wettrennen {
 				break;
 			}
 		}
-		
-		
-		// Position zuordnen (mit sortieren !!!)
-		Collections.sort(boote);
-		
-		zaehler = 0;
-		for (Boot temp : boote) {
-			++zaehler;
-			temp.setPosition(zaehler);
-		}
-		
 
-		
-		// Sortier-Schalter auf True, da Sortiert wurde:
+		// Werte zuordnen:
 		for (int i = 0; i < amountOfBoats; i++) {
-			boote.get(i).setSortierSchalter(true);
-			}
-		
 
-		// Zurücksortiert nach Bahn...
-		Collections.sort(boote);
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+			int randomFehlstart = (int) (Math.random() * 2 + 1);
+			if (randomFehlstart == 1) {
+				boote.get(i).setFehlstart(true);
+			}
+
+			int randomLeistungsEinbruch = (int) (Math.random() * 2 + 1);
+			if (randomLeistungsEinbruch == 1) {
+				boote.get(i).setLeistungsEinbruch(true);
+			}
+
+			int randomZwischenspurt = (int) (Math.random() * 2 + 1);
+			if (randomZwischenspurt == 1) {
+				boote.get(i).setZwischenspurt(true);
+			}
+
+			int randomEndspurt = (int) (Math.random() * 2 + 1);
+			if (randomEndspurt == 1 && randomLeistungsEinbruch == 2) {
+				boote.get(i).setEndspurt(true);
+			}
+		}
+
 		// erstelle Rennstrecke
 		ArrayList<Rennstrecke> rennstrecke1 = new ArrayList<>();
 		for (int i = 0; i < amountOfBoats; i++) {
 			rennstrecke1.add(new Rennstrecke(amountOfBoats, lengthOfRacingField));
 		}
-		
+
 		// erstelle RennstreckeString
 		for (int i = 0; i < amountOfBoats; i++) {
 			rennstrecke1.get(i).rennstreckeErstellen();
-			
+
 			// Startposition Boot:
-			boote.get(i).setxPos(boote.get(i).getLengthOfBoot()+ausgleichsfaktor);
+			boote.get(i).setxPos(boote.get(i).getLengthOfBoot() + ausgleichsfaktor);
 			boote.get(i).setBahnNummer(boote.get(i).getbahnNummer());
-			
+
 			// Boote in Rennstrecke einsetzen
 			boote.get(i).bootEinsetzen(rennstrecke1.get(i));
-			
 		}
+
 		
-		
-		
-		factorForSpeedOfBoat = factorForSpeedOfBoat + 5*amountOfRuderer; //damit alle Boote ungefähr gleich lange benötigen
-		
-		k=6;
+		factorForSpeedOfBoat = factorForSpeedOfBoat + 5 * amountOfRuderer; // damit alle Boote ungefähr gleich lange
+																			// benötigen
+
+		k = 6;
 		// Start of the race
 		do {
 			k--;
 			System.out.println(" ");
 			System.out.println(" ");
-			System.out.println(" Countdown: " +k);
-			
-			
+			System.out.println(" Countdown: " + k);
+
 			for (int i = 0; i < amountOfBoats; i++) {
 				if (i == 0) {
 					System.out.println(rennstrecke1.get(i).getRandStringBahn());
@@ -184,99 +170,123 @@ public class Wettrennen {
 					System.out.println(rennstrecke1.get(i).getRandStringBahn());
 				} else {
 					System.out.println(rennstrecke1.get(i).getRandStringMiddle());
-				}	
+				}
 			}
-			
-			//slowing down time...
+
+			// slowing down time...
 			Thread.sleep(1000, 0);
-			
-			
-		} while (k>0);
-				
-			
+
+		} while (k > 0);
+
+		
 		
 		// Zeige Rennstrecke
-		k=0;
+		k = 0;
 		boolean ende = false;
-		
+
 		do {
 			k++;
 			String goString = ">";
-			//System.out.println("k: " +k);
+			// System.out.println("k: " +k);
 			System.out.println(" ");
 			System.out.println(" ");
-			if (k<9) {
-				System.out.println(" >" + goString.repeat(k/2) + " GO! ");
-				//System.out.println(" GO " +goString.repeat(k/2) +">");
+			if (k < 9) {
+				System.out.println(" >" + goString.repeat(k / 2) + " GO! ");
+				// System.out.println(" GO " +goString.repeat(k/2) +">");
 			} else {
 				System.out.println(" ");
 			}
-			
-			
-			
+
 			// Print Bahn mit Boot
 			for (int i = 0; i < amountOfBoats; i++) {
 				if (i == 0) {
 					System.out.println(rennstrecke1.get(i).getRandStringBahn());
 				}
 				System.out.println(rennstrecke1.get(i).getBackboardWater());
-				System.out.println(rennstrecke1.get(i).getMiddleStringBoat() + " " + boote.get(i).getPosition() + ".");
+
+				// in der ersten Runde noch keine Position ausgeben (da vorher nicht mit
+				// Set-Bestätigt...!
+				if (k == 1) {
+					System.out.println(rennstrecke1.get(i).getMiddleStringBoat());
+				} else {
+					System.out.println(
+							rennstrecke1.get(i).getMiddleStringBoat() + " " + boote.get(i).getPosition() + ".");
+				}
+
 				System.out.println(rennstrecke1.get(i).getStarboardWater());
 				if (i == amountOfBoats - 1) {
 					System.out.println(rennstrecke1.get(i).getRandStringBahn());
 				} else {
 					System.out.println(rennstrecke1.get(i).getRandStringMiddle());
-				}	
+				}
 			}
 			// neue Position von Booten
 			for (int j = 0; j < amountOfBoats; j++) {
-				
+
 				double leistungBoot = boote.get(j).getLeistungBoot();
-				double sqrtTeilstrecke = Math.sqrt(leistungBoot)/factorForSpeedOfBoat; //Leistung umgerechnet in Strecke (pro Schritt)
-				double teilstreckeMitFaktor = sqrtTeilstrecke * boote.get(j).luckFaktor();
-			
-				//System.out.println("Strecke Made: " + boote.get(j).getStreckeMade());
-				//System.out.println("sqrtTeilstrecke: " +sqrtTeilstrecke);
+				double sqrtTeilstrecke = Math.sqrt(leistungBoot) / factorForSpeedOfBoat; // Leistung umgerechnet in
+																						 // Strecke (pro Schritt)
+				double teilstreckeMitFaktor = sqrtTeilstrecke * boote.get(j).luckFaktor(lengthOfRacingField);
+				//System.out.println(boote.get(j).luckFaktor(lengthOfRacingField) + ": Lucky-Faktor");
+
 				
-				//streckeGesamt muss am Anfang immer mindestens 1 sein!
+
+				// streckeGesamt muss am Anfang immer mindestens 1 sein!
 				boote.get(j).setStreckeMade(teilstreckeMitFaktor + boote.get(j).getStreckeMade());
-				int streckeGesamt = (int) boote.get(j).getStreckeMade() +ausgleichsfaktor;
+				int streckeGesamt = (int) boote.get(j).getStreckeMade() + ausgleichsfaktor;
 				if (streckeGesamt <= 1) {
 					streckeGesamt = 1;
 				}
-				
-				
+
 				// Abfrage START
 				int varZwischen = streckeGesamt + boote.get(j).getLengthOfBoot();
-				if (streckeGesamt + boote.get(j).getLengthOfBoot() >= lengthOfRacingField+ausgleichsfaktor) {
+				if (streckeGesamt + boote.get(j).getLengthOfBoot() >= lengthOfRacingField + ausgleichsfaktor) {
 					streckeGesamt = lengthOfRacingField - boote.get(j).getLengthOfBoot() + ausgleichsfaktor;
 				}
-				
-				//x-Position SET...
-				boote.get(j).setxPos(boote.get(j).getxPos()+(streckeGesamt - (boote.get(j).getxPos()-boote.get(j).getLengthOfBoot())));
-				
+
+				// x-Position SET...
+				boote.get(j).setxPos(boote.get(j).getxPos()
+						+ (streckeGesamt - (boote.get(j).getxPos() - boote.get(j).getLengthOfBoot())));
+
 				boote.get(j).bootEinsetzen(rennstrecke1.get(j));
-				
-				
-				//Abfrage, falls rechter Rand...!
-				if (boote.get(j).getStreckeMade() >= lengthOfRacingField - boote.get(j).getLengthOfBoot()){	
+
+				// Abfrage, falls rechter Rand...!
+				if (boote.get(j).getStreckeMade() >= lengthOfRacingField - boote.get(j).getLengthOfBoot()) {
 					boote.get(j).setWinner(true);
 					if (laengsteDistanzImRennen < boote.get(j).getStreckeMade()) {
 						laengsteDistanzImRennen = boote.get(j).getStreckeMade();
 					}
 					ende = true;
-				}				
-			}
-			
-			//slowing down time...
-			Thread.sleep(400, 0);
-			
-		} while (!ende);
-		
+				}
 
-		
-		
-		//Who is the winner?
+				
+
+				// Markus INPUT !!!!!!!!!!!!
+				// AnonymeKlasse
+				//
+				// Position zuordnen (mit sortieren !!!)
+				List<Boot> gerankteBoote = new ArrayList<Boot>(boote);
+				Collections.sort(gerankteBoote, new Comparator<Boot>() {
+					@Override
+					public int compare(Boot boot1, Boot boot2) {
+						return Double.valueOf(boot2.getStreckeMade()).compareTo(boot1.getStreckeMade());
+					}
+				});
+
+				zaehler = 0;
+				for (Boot temp : gerankteBoote) {
+					++zaehler;
+					temp.setPosition(zaehler);
+				}
+
+			}
+
+			// slowing down time...
+			Thread.sleep(400, 0);
+
+		} while (!ende);
+
+		// Who is the winner?
 		for (int j = 0; j < amountOfBoats; j++) {
 			if (boote.get(j).getStreckeMade() >= laengsteDistanzImRennen) {
 				boote.get(j).setWinner(true);
@@ -284,23 +294,22 @@ public class Wettrennen {
 				boote.get(j).setWinner(false);
 			}
 		}
-		
-		
-		
-		//finischer Bild
+
+		// finischer Bild
 		System.out.println(" ");
 		System.out.println(" ");
 		System.out.println(" THE END!");
-		
+
 		for (int i = 0; i < amountOfBoats; i++) {
 			if (i == 0) {
 				System.out.println(rennstrecke1.get(i).getRandStringBahn());
 			}
 			System.out.println(rennstrecke1.get(i).getBackboardWater());
-			
-			//boote.get(i).setWinner(true);
+
+			// boote.get(i).setWinner(true);
 			if (boote.get(i).isWinner()) {
-				System.out.println(rennstrecke1.get(i).getMiddleStringBoat() + boote.get(i).getStreckeMade() + "  --> Winner !!!");
+				System.out.println(
+						rennstrecke1.get(i).getMiddleStringBoat() + boote.get(i).getStreckeMade() + "  --> Winner !!!");
 			} else {
 				System.out.println(rennstrecke1.get(i).getMiddleStringBoat() + boote.get(i).getStreckeMade());
 			}
@@ -309,9 +318,9 @@ public class Wettrennen {
 				System.out.println(rennstrecke1.get(i).getRandStringBahn());
 			} else {
 				System.out.println(rennstrecke1.get(i).getRandStringMiddle());
-			}	
-		}		
-		
+			}
+		}
+
 	}
 
 }
